42e7b9a6753b4969341b8f7a0a0b3a9aa5b9c646
michaelspace <kmichal1797@gmail.com>
2018-05-23 22:24:26 +0200

    Added data model

---
+++ ASP.NETCoreSimpleApp/Data/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreSimpleApp.Data.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ ASP.NETCoreSimpleApp/Data/Model/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreSimpleApp.Data.Model
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+
+        public virtual Director Director { get; set; }
+        public int DirectorId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
